d16e8d3f792eb2f5b61ae2cc29ed27fdf893c350
skaszi70 <skaszi70@gmail.com>
2025-04-26 19:24:48 +0200

    Update generic.mk

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -1,5 +1,6 @@
 define Device/mikrotik_rb711
   DEVICE_TITLE := MikroTik RB711
+  DEVICE_DTS := mikrotik_rb711
   DEVICE_PACKAGES := kmod-ath9k kmod-usb-core kmod-usb2 kmod-usb-ohci kmod-usb-ehci luci snmpd
   BOARDNAME := rb711
   IMAGE_SIZE := 16256k
